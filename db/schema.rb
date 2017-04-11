cfdf353791b2dbd60046ef78404a994943ffa3ed
chentianwei <ctw.3@163.com>
2017-04-11 10:53:24 +0800

    add model post

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20170411024826) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
-    t.string   "user_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
