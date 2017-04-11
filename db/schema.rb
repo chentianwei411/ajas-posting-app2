02d9ac76fed2e68894bd22391e39e19da5ab87b7
chentianwei <ctw.3@163.com>
2017-04-11 10:52:12 +0800

    add model post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411023856) do
+ActiveRecord::Schema.define(version: 20170411024826) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.string   "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
